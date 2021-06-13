c2563ac261684110c4859e5bb12d4069988f679e
Cameron Clark <cameron@gremlin.com>
2021-06-13 18:25:53 -0400

    Create certs without prompt and add build step to create certs

--- app/pki/create_cert.sh
+++ app/pki/create_cert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+set -x
 
-openssl genrsa -out key.pem 2048
-chmod 600 server.key
-openssl req -new -key key.pem -out server.csr
+openssl req -nodes -newkey rsa:2048 -keyout key.pem -out server.csr -subj "/C=US/ST=NY/L=NYC/O=idpy/OU=idpy/CN=idpy.com"
+chmod 600 key.pem
 openssl x509 -req -days 365 -in server.csr -signkey key.pem -out cert.pem
